d8be439a58565ff105401aaf3b213956328e1412
mr-arzh <asiris.joy@icloud.com>
2025-01-23 15:59:36 +0300

    added some data to DB and improved methods in PlayerRepositoryDB class

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,9 +1,11 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
@@ -15,43 +17,55 @@ import java.util.Properties;
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
 
-    private final SessionFactory sessionFactory;;
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
         Properties properties = new Properties();
         properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
+        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-        properties.put(Environment.SHOW_SQL, "true");
+        //properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.USER, "root");
         properties.put(Environment.PASS, "root");
         properties.put(Environment.HBM2DDL_AUTO, "update");
         sessionFactory = new Configuration()
-                .setProperties(Properties properties)
+                .setProperties(properties)
                 .addAnnotatedClass(Player.class)
                 .buildSessionFactory();
     }
 
-    Properties properties = new Properties();
-//        properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
-//        properties.put(Environment.URL, "jdbc:mysql://localhost:3306/test");
-//        properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
-//        properties.put(Environment.SHOW_SQL, "true");
-//        properties.put(Environment.USER, "root");
-//        properties.put(Environment.PASS, "root");
-//        properties.put(Environment.HBM2DDL_AUTO, "update");
-    properties.put
 
 
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+//        Query<Player> query = session.createNativeQuery("select * from rpg.player limit = ':pageNumber' offset ':pageSize'", Player.class );
+//        query.setParameter("pageNumber", pageNumber);
+//        query.setParameter("pageSize", pageSize);
+//        List<Player> players = query.getResultList();
+//        session.getTransaction().commit();
+//        session.close();
+
+        Query<Player> query = session.createQuery("from rpg.player", Player.class);
+        query.setFirstResult(pageNumber * pageSize);
+        query.setMaxResults(pageSize);
+        List<Player> players = query.getResultList();
+        session.getTransaction().commit();
+        session.close();
+        return players;
     }
 
     @Override
     public int getAllCount() {
-        return 0;
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query<Long> query = session.createQuery("select count(*) from rpg.player", Long.class);
+        Long result =  query.getSingleResult();
+        session.getTransaction().commit();
+        session.close();
+        return result.intValue();
     }
 
     @Override
@@ -76,6 +90,7 @@ public class PlayerRepositoryDB implements IPlayerRepository {
 
     @PreDestroy
     public void beforeStop() {
+        sessionFactory.close();
 
     }
 }
